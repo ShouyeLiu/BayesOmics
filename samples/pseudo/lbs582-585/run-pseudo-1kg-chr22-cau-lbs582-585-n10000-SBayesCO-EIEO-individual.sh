22356de1469121725459b43735977adaf9ecc19f
Shouye Liu <syliu.xue@foxmail.com>
2026-09-15 18:31:32 +0800

    in demo, use thort chain

--- samples/pseudo/lbs582-585/run-pseudo-1kg-chr22-cau-lbs582-585-n10000-SBayesCO-EIEO-individual.sh
+++ samples/pseudo/lbs582-585/run-pseudo-1kg-chr22-cau-lbs582-585-n10000-SBayesCO-EIEO-individual.sh
@@ -12,9 +12,9 @@ cd "$example_dir/data"
 "$binary" --mcmc-type EIEO --genotype-scale pseudo-1kg-chr22-cau-lbs582-585-n10000-genotype-scale.txt \
   --hsq .5 --hsq-cis .5 \
   --pi-genic .4 --pi-genic-gwas .4 --pi-genic-xqtl .4 --pi-intergenic .01 \
-  --chain-length 3000 --burn-in 2000 --thin 1 --seed 20260914 \
+  --chain-length 1000 --burn-in 600 --thin 1 --seed 20260914 \
   --thread 1 --memory auto --genotype-storage auto \
-  --write-mcmc-txt --out-freq 3000 \
+  --write-mcmc-txt --out-freq 100 \
   --bayes CO --bfile pseudo-1kg-chr22-cau-lbs582-585-n10000 --pheno pseudo-1kg-chr22-cau-lbs582-585-n10000-gwas.phen \
   --gene pseudo-1kg-chr22-cau-lbs582-585-n10000-molecular.plist --gene-snp-map pseudo-1kg-chr22-cau-lbs582-585-n10000-model-snp-gene-map.txt \
   --out "$output/models/pseudo-1kg-chr22-cau-lbs582-585-n10000-SBayesCO-EIEO-individual"
--- samples/pseudo/lbs582-585/run-pseudo-1kg-chr22-cau-lbs582-585-n10000-SBayesCO-EIEO-summary.sh
+++ samples/pseudo/lbs582-585/run-pseudo-1kg-chr22-cau-lbs582-585-n10000-SBayesCO-EIEO-summary.sh
@@ -12,9 +12,9 @@ cd "$example_dir/data"
 "$binary" --mcmc-type EIEO --genotype-scale pseudo-1kg-chr22-cau-lbs582-585-n10000-genotype-scale.txt \
   --hsq .5 --hsq-cis .5 \
   --pi-genic .4 --pi-genic-gwas .4 --pi-genic-xqtl .4 --pi-intergenic .01 \
-  --chain-length 3000 --burn-in 2000 --thin 1 --seed 20260914 \
+  --chain-length 1000 --burn-in 600 --thin 1 --seed 20260914 \
   --thread 1 --memory auto --genotype-storage auto \
-  --write-mcmc-txt --out-freq 3000 \
+  --write-mcmc-txt --out-freq 100 \
   --sbayes CO --gwas-summary pseudo-1kg-chr22-cau-lbs582-585-n10000-gwas.ma --beqtl-summary-gz pseudo-1kg-chr22-cau-lbs582-585-n10000-eqtl \
   --ldm-eigen "$output/pseudo-1kg-chr22-cau-lbs582-585-n10000-ld-blocks" --ldm-eigen-gene "$output/pseudo-1kg-chr22-cau-lbs582-585-n10000-molecular-ld" \
   --ldm-eigen-cutoff 1 --ldm-eigen-gene-cutoff 1 --ldm-correlation \
